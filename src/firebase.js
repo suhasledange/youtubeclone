e8a4c60ffbf9abd7709e6ea936441576e77c8b7e
suhasledange <suhasledange10@gmail.com>
2024-02-15 16:55:58 +0530

    youtube

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAQ91fLStuBYa5XvsHNUQxTEESLEVHb1no",
+    authDomain: "clone-b2162.firebaseapp.com",
+    projectId: "clone-b2162",
+    storageBucket: "clone-b2162.appspot.com",
+    messagingSenderId: "842943172547",
+    appId: "1:842943172547:web:ede403e1ac957fd1dd3776",
+    measurementId: "G-MM17YRFKKE"
+  };
